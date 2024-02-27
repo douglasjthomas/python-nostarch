681376515ab2d7a51771c13eedc37ff00a25822d
douglasjthomas <122920118+douglasjthomas@users.noreply.github.com>
2024-02-26 21:54:54 -0700

    Test lesson

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#message = "Hello Python World!"
+#print(message)
+
+#message = "Hello Python Crash Course world!"
+#print(message)
+
+
+#first_name = "ada"
+#last_name = "lovelace"
+#full_name = f"{first_name} {last_name}"
+#print(f"Hello, {full_name.title()}!")
+
+spacetext = "this has a space at the end "
+print(spacetext.rstrip())
+print(spacetext.removeprefix('this has '))
